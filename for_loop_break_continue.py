e43423acde26d9edfb835e8c689f0a8e53bd207c
reajane262 <domingoreajane_bsit@plmun.edu.ph>
2025-02-17 21:19:34 -0800

    Update for_loop_break_continue.py

--- for_loop_break_continue.py
+++ for_loop_break_continue.py
@@ -8,11 +8,3 @@ for num in numbers:
         break  # Exit the loop completely
     
     print(num)
-
-nested_for_loop_multiplication_table.py
-
-for i in range(1, 6):
-    for j in range(1, 6):  # Indent this loop!
-        product = i * j  # Indent this line!
-        print(f"{product:4}", end="")  # Indent this line!
-    print()  # Indent this line! fix this code
